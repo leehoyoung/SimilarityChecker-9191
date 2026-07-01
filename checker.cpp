2c217c81f0c355e6547ad3aa90a1e223702719a8
Hoyoung Lee <hyl4237@gmail.com>
2026-07-01 16:35:37 +0900

    [refactor] Extract ratio/score calculation into private calculateScore

--- checker.cpp
+++ checker.cpp
@@ -8,8 +8,12 @@ public:
 		int longSize = std::max(a.size(), b.size());
 		int shortSize = std::min(a.size(), b.size());
 
+		return calculateScore(longSize, shortSize);
+	}
+
+private:
+	int calculateScore(int longSize, int shortSize) {
 		double ratio = 1 - static_cast<double>(longSize - shortSize) / shortSize;
 		return static_cast<int>(ratio * MAX_SCORE);
 	}
-
 };
